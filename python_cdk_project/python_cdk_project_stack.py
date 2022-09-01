9458cfbe7c3eb152bed6185bddf5b57fe92c9c4e
Alex Hardman <alexhardman@launchdarkly.com>
2022-09-01 09:58:25 -0400

    working to get bundling right

--- python_cdk_project/python_cdk_project_stack.py
+++ python_cdk_project/python_cdk_project_stack.py
@@ -31,9 +31,9 @@ class PythonCdkProjectStack(Stack):
         # )
 
         lambda_alpha.PythonFunction(self, "munnawars-function",
-                                    entry="./",  # required
+                                    entry="./munnawar_python_function/",  # required
                                     runtime=lambda_.Runtime.PYTHON_3_8,  # required
-                                    index="munnawar_python_function.py",  # optional, defaults to 'index.py'
+                                    index="index.py",  # optional, defaults to 'index.py'
                                     handler="show_message",
                                     environment={'SDK_KEY': sdk_key,
                                                  'FLAG_KEY': feature_flag_key}
