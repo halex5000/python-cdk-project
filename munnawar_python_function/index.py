d503faacb74dfae3dac11fdacc9b6c2a406209f8
Alex Hardman <alexhardman@launchdarkly.com>
2022-09-13 10:43:49 -0400

    adding example JSON and updating function logic

--- munnawar_python_function/index.py
+++ munnawar_python_function/index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from operator import itemgetter, or_
 import os
 import time
 import random
@@ -15,13 +16,8 @@ sdk_key = os.getenv('SDK_KEY') or ''
 # Set feature_flag_key to the feature flag key you want to evaluate
 feature_flag_key = os.getenv('FLAG_KEY') or ''
 
-wait = 2
-error_rate_max = 3
-error_rate_min = 1
-error_rate_bump = 3
-success_rate_max = 15
-success_rate_min = 10
-iterations = 10
+
+launch_darkly_client = None
 
 
 def show_message(s):
@@ -30,74 +26,66 @@ def show_message(s):
 
 
 def handler(event, context):
-    if not sdk_key:
-        show_message(
-            "Please edit test.py to set sdk_key to your LaunchDarkly SDK key first")
-        exit()
+    # print(event)
 
-    ldclient.set_config(Config(sdk_key))
+    global launch_darkly_client
+
+    if launch_darkly_client is None:
+        if not sdk_key:
+            show_message(
+                "Please edit test.py to set sdk_key to your LaunchDarkly SDK key first")
+            exit()
+
+        ldclient.set_config(Config(sdk_key))
+
+        launch_darkly_client = ldclient.get()
 
     total_success = 0
     total_failure = 0
+
+    error_rate_max, error_rate_min, error_rate_bump, success_rate_max, success_rate_min, wait_time, iterations = itemgetter('errorRateMax', 'errorRateMin', 'errorRateBump',
+                                                                                                                            'successRateMax', 'successRateMin', 'waitSeconds', 'iterations')(event)
+    error_rate_max = error_rate_max or 3
+    error_rate_min = error_rate_min or 1
+    error_rate_bump = error_rate_bump or 3
+    success_rate_max = success_rate_max or 15
+    success_rate_min = success_rate_min or 10
+    iterations = iterations+1 or 1
+
     # The SDK starts up the first time ldclient.get() is called
-    if ldclient.get().is_initialized():
+    if launch_darkly_client.is_initialized():
         show_message("SDK successfully initialized!")
 
-        for x in range(iterations):
-            # Set up the user properties. This user should appear on your LaunchDarkly users dashboard
-            # soon after you run the demo.
-            user = {
-                "key": "munnawar-unique-user-key",
-                "name": "Munnawar"
-            }
+        # Set up the user properties. This user should appear on your LaunchDarkly users dashboard
+        # soon after you run the demo.
+        user = {
+            "key": "munnawar-unique-user-key",
+            "name": "Munnawar"
+        }
 
-            flag_value = ldclient.get().variation(feature_flag_key, user, 'default message')
+        flag_value = launch_darkly_client.variation(
+            feature_flag_key, user, 'default message')
 
-            # show_message("Feature flag '%s' is %s for this user" %
-            #              (feature_flag_key, flag_value))
+        good = random.randint(success_rate_min, success_rate_max)
 
-            # increment the 200 status code (the good)
-            # no matter the flag state
-
-            good = random.randint(success_rate_min, success_rate_max)
-
-            # if flag status is GOOD
-            if (flag_value == 'Buongiorno'):
-                # send XX value to the lambda metric
-                bad = random.randint(error_rate_min, error_rate_max)
-                lambda_metric('success', good)
-                lambda_metric('failure', bad)
-                print('reporting good: ' + str(good) + ' bad: ' + str(bad))
-                total_success += good
-                total_failure += bad
-            # continue the while
-            else:
-                bad = random.randint(
-                    error_rate_min + error_rate_bump, error_rate_max + error_rate_bump)
-                lambda_metric('success', good)
-                lambda_metric('failure', bad)
-                print('reporting good: ' + str(good) + ' bad: ' + str(bad))
-                total_success += good
-                total_failure += bad
-
-            # sleep a little
-            time.sleep(wait)
-
-            # if the flag_state is ON
-            # break the while loop and enter a new while loop where it increments
-            # bad things
+        if flag_value:  # if the feature is on
+            bad = random.randint(
+                error_rate_min + error_rate_bump, error_rate_max + error_rate_bump)
+            lambda_metric('success', good)
+            lambda_metric('failure', bad)
+            # print('reporting good: ' + str(good) + ' bad: ' + str(bad))
+            total_success += good
+            total_failure += bad
+        else:  # if the feature is off
+            # send XX value to the lambda metric
+            bad = random.randint(error_rate_min, error_rate_max)
+            lambda_metric('success', good)
+            lambda_metric('failure', bad)
+            # print('reporting good: ' + str(good) + ' bad: ' + str(bad))
+            total_success += good
+            total_failure += bad
 
     else:
         raise Exception("SDK failed to initialize")
 
-    # Here we ensure that the SDK shuts down cleanly and has a chance to deliver analytics
-    # events to LaunchDarkly before the program exits. If analytics events are not delivered,
-    # the user properties and flag usage statistics will not appear on your dashboard. In a
-    # normal long-running application, the SDK would continue running and events would be
-    # delivered automatically in the background.
-    # ldclient.get().close()
-
-    return {
-        'total_success': total_success,
-        'total_failure': total_failure
-    }
+    return iterations
--- python_cdk_project/python_cdk_project_stack.py
+++ python_cdk_project/python_cdk_project_stack.py
@@ -5,7 +5,9 @@ from aws_cdk import (
     Stack,
     # aws_sqs as sqs,
     aws_lambda as lambda_,
-    aws_lambda_python_alpha as lambda_alpha
+    aws_lambda_python_alpha as lambda_alpha,
+    aws_stepfunctions as state_machines,
+    aws_stepfunctions_tasks as tasks
 )
 from constructs import Construct
 from dotenv import load_dotenv
@@ -38,21 +40,47 @@ class PythonCdkProjectStack(Stack):
                           api_key=datadog_api_key,
                           )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "PythonCdkProjectQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
-
         munnawarPythonFunction = lambda_alpha.PythonFunction(self, "munnawars-function",
                                                              entry="./munnawar_python_function/",  # required
                                                              runtime=lambda_.Runtime.PYTHON_3_8,  # required
                                                              index="index.py",  # optional, defaults to 'index.py'
                                                              handler="handler",
-                                                             timeout=Duration.seconds(
-                                                                 60),
                                                              environment={'SDK_KEY': sdk_key,
                                                                           'FLAG_KEY': feature_flag_key}
                                                              )
 
         datadog.add_lambda_functions([munnawarPythonFunction])
+
+        make_stats = tasks.LambdaInvoke(self, "Make Stats",
+                                        lambda_function=munnawarPythonFunction,  # type: ignore
+                                        result_path="$.iterations",
+                                        payload_response_only=True,
+                                        # payload=state_machines.TaskInput.from_object({
+                                        #     "$.errorRateMax": state_machines.JsonPath.number_at("$.errorRateMax"),
+                                        #     "$.errorRateMin": state_machines.JsonPath.number_at("$.errorRateMin"),
+                                        #     "$.errorRateBump": state_machines.JsonPath.number_at("$.errorRateBump"),
+                                        #     "$.successRateMax": state_machines.JsonPath.number_at("$.successRateMax"),
+                                        #     "$.successRateMin": state_machines.JsonPath.number_at("$.successRateMin"),
+                                        #     "$.waitSeconds": state_machines.JsonPath.number_at("$.waitSeconds"),
+                                        #     "$.iterations": state_machines.JsonPath.number_at("$.iterations")
+                                        # })
+                                        )
+
+        job_success = state_machines.Succeed(self, "Success")
+
+        wait_x = state_machines.Wait(self, "Wait X Seconds",
+                                     time=state_machines.WaitTime.seconds_path(
+                                         "$.waitSeconds")
+                                     ).next(make_stats)
+
+        yo_we_finished_yet = state_machines.Choice(self, 'check if we have done too many iterations').when(
+            state_machines.Condition.number_greater_than_equals("$.iterations", 1000), job_success).otherwise(wait_x)
+
+        definition = make_stats.next(yo_we_finished_yet)
+
+        # Create state machine
+        sm = state_machines.StateMachine(
+            self, "munnawar-datadog-statemachine",
+            definition=definition,
+            timeout=Duration.minutes(30),
+        )
